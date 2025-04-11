a14f783c9eb072899641221795446a7f6a6f3049
Heinrich-Ressler <hprintr@gmail.com>
2025-04-11 10:11:34 +0200

    изменения в docker-entrypoint.sh во всех сервисах и изменение моделей в субскрипшен

--- team-chatty2/subscription_service/models.py
+++ team-chatty2/subscription_service/models.py
@@ -5,33 +5,6 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from db_base import Base  # Импортируем Base из db_base.py
 
-# Модель пользователя
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
-    nickname = Column(String, unique=True, nullable=False)
-    role = Column(String, nullable=False, default="user")
-
-    # Связь: один пользователь — много постов
-    posts = relationship("Post", back_populates="author")
-
-
-# Модель поста
-class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    title = Column(String, index=True)
-    content = Column(Text)
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-    # Связь с автором поста
-    author = relationship("User", back_populates="posts")
-
 
 class Subscription(Base):
     __tablename__ = "subscriptions"
